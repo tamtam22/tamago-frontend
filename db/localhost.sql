127f00cb302672972a6d701163e55f3948d955dc
Zach <zach.junwei@hotmail.com>
2015-10-07 18:11:22 +0800

    Updated table structure and added sample data

--- db/localhost.sql
+++ db/localhost.sql
@@ -1,11 +1,12 @@
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
 
-CREATE DATABASE IF NOT EXISTS `cms` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+CREATE DATABASE IF NOT EXISTS `cms` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE `cms`;
 
 DROP TABLE IF EXISTS `incidents`;
@@ -15,11 +16,50 @@ CREATE TABLE IF NOT EXISTS `incidents` (
   `mobile` int(8) NOT NULL,
   `latitude` double NOT NULL,
   `longitude` double NOT NULL,
-  `assistanceType` varchar(10) NOT NULL,
-  `reportedOn` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `assistance_type` varchar(10) NOT NULL,
+  `reported_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `last_updated_on` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
   `operator` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `operator` (`operator`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+INSERT INTO `incidents` (`id`, `name`, `mobile`, `latitude`, `longitude`, `assistance_type`, `reported_on`, `last_updated_on`, `operator`) VALUES
+(1, 'Tan Ah Meng', 99991111, 1.3340635073290337, 103.83099398950196, '1', '2015-10-07 09:54:55', '2015-10-07 10:07:39', 2);
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `user_type` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_type` (`user_type`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `user_type`) VALUES
+(1, 'Administrator', 'admin@admin.com', '827ccb0eea8a706c4c34a16891f84e7b', 1),
+(2, 'Mary Chia', 'marry@ops.com', '827ccb0eea8a706c4c34a16891f84e7b', 2);
+
+DROP TABLE IF EXISTS `users_type`;
+CREATE TABLE IF NOT EXISTS `users_type` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+
+INSERT INTO `users_type` (`id`, `name`) VALUES
+(1, 'Administrator'),
+(2, 'Call Center Operator'),
+(3, 'Government Agencies');
+
+
+ALTER TABLE `incidents`
+  ADD CONSTRAINT `incidents_ibfk_1` FOREIGN KEY (`operator`) REFERENCES `users` (`id`);
+
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`user_type`) REFERENCES `users_type` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
